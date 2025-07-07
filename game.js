7754caf4f61670aa3c9b99142c4674c8039a962a
Teng-0822 <167068953+Teng-0822@users.noreply.github.com>
2025-07-07 21:45:51 +0800

    Update and rename app.js to game.js

---
+++ game.js
@@ -0,0 +1,14 @@
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyDv0_wV_7U_akaYMCMmqIuZU7mFHu4oGv0",
+  authDomain: "firebullz-c5da5.firebaseapp.com",
+  databaseURL: "https://firebullz-c5da5-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "firebullz-c5da5",
+  storageBucket: "firebullz-c5da5.appspot.com",
+  messagingSenderId: "209013988107",
+  appId: "1:209013988107:web:4f323d4f66bd167781200e",
+  measurementId: "G-ZTVNCF4BQ6"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
